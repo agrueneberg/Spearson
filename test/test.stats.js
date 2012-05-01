370d0a626cbb882b1d5e62448da9ea6b3da8704f
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-05-01 13:06:40 -0500

    Add sum test.

--- test/test.stats.js
+++ test/test.stats.js
@@ -1,3 +1,13 @@
 describe("Stats", function () {
 
+    describe("sum", function () {
+        it("the sum of [1,2,3,4,5] is 15", function () {
+         // Compare to MATLAB: sum([1 2 3 4 5])
+         // Compare to R: sum(c(1,2,3,4,5))
+            var res;
+            res = stats.sum([1, 2, 3, 4, 5]);
+            expect(res).to.equal(15);
+        });
+    });
+
 });
