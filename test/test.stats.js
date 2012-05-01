936c8ce9d88874c4af884b2467790102bbb64e8f
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-05-01 13:16:05 -0500

    Add Spearman correlation coefficient test.

--- test/test.stats.js
+++ test/test.stats.js
@@ -62,6 +62,15 @@ describe("Stats", function () {
                 expect(res).to.equal(-0.6181);
             });
         });
+        describe("spearman", function () {
+            it("the Spearman correlation coefficient of [1,7,2,3,4] and [9,3,5,2,1] is -0.7", function () {
+             // Compare to MATLAB: corr([1;7;2;3;4], [9;3;5;2;1], 'type', 'Spearman')
+             // Compare to R: cor(x=c(1,7,2,3,4), y=c(9,3,5,2,1), method="spearman")
+                var res;
+                res = stats.correlation.spearman([1, 7, 2, 3, 4], [9, 3, 5, 2, 1]);
+                expect(res).to.equal(-0.7);
+            });
+        });
     });
 
 });
