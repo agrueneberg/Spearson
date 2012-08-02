37a1da1e2fc823ca1ccb5d60c56437ac14aef796
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-08-02 14:28:04 -0500

    Mixed up mean and median in tests. *facepalm*

--- test/test.spearson.js
+++ test/test.spearson.js
@@ -67,21 +67,21 @@ describe("Spearson", function () {
          // Compare to MATLAB: median([1])
          // Compare to R: median(c(1))
             var res;
-            res = spearson.mean([1]);
+            res = spearson.median([1]);
             expect(res).to.equal(1);
         });
         it("the median of [1,2,3,4,5] is 3", function () {
          // Compare to MATLAB: median([1 2 3 4 5])
          // Compare to R: median(c(1,2,3,4,5))
             var res;
-            res = spearson.mean([1, 2, 3, 4, 5]);
+            res = spearson.median([1, 2, 3, 4, 5]);
             expect(res).to.equal(3);
         });
         it("the median of [1,2,3,4,5,6] is 3.5", function () {
          // Compare to MATLAB: median([1 2 3 4 5 6])
          // Compare to R: median(c(1,2,3,4,5,6))
             var res;
-            res = spearson.mean([1, 2, 3, 4, 5, 6]);
+            res = spearson.median([1, 2, 3, 4, 5, 6]);
             expect(res).to.equal(3.5);
         });
     });
